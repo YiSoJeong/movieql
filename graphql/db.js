cf37c8b9f4bc51c72fb3584cd3f2bd1e2be48edc
sojeong <soj980312@gmail.com>
2019-07-31 17:14:04 +0900

    Extend the schema

---
+++ graphql/db.js
@@ -0,0 +1,31 @@
+export const people = [
+    {
+        id: "0",
+        name: "Sso",
+        age: 20,
+        gender: "female"
+    },
+    {
+        id: "1",
+        name: "Jung",
+        age: 22,
+        gender: "female"
+    },
+    {
+        id: "2",
+        name: "Moon",
+        age: 25,
+        gender: "male"
+    },
+    {
+        id: "3",
+        name: "Choi",
+        age: 30,
+        gender: "male"
+    }
+];
+
+export const getById = id => {
+    const filteredPeople = people.filter(person => id === id);
+    return filteredPeople[0]
+}
